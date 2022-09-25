28a8197278f3caf0e3070cfdec87aefef240fc80
ortigolu <vpasuenolar@gmail.com>
2022-09-25 18:58:17 -0400

    first commit

---
+++ ReservasHotel/src/main/java/io/Sheraton/Reservas/ReservasHotelApplication.java
@@ -0,0 +1,13 @@
+package io.Sheraton.Reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasHotelApplication.class, args);
+	}
+
+}
---
+++ ReservasHotel/src/test/java/io/Sheraton/Reservas/ReservasHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package io.Sheraton.Reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
